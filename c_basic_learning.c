e53a409804540623a10ad2f6ffb2b63e28954dd9
Suri Xing <surixing@outlook.com>
2024-08-26 22:14:21 +0800

    Today I learnt the difference between "=" and "==".
    "=" means we initialize a variable or assign a new value to an existing variable.
    "==" means the comparision between the variable we initialized and something else.
    "!=" is the opposite operation of "==".
    
    One more thing, if we type "int c;" without initialized value, c could be anything! It's very intersting how the computer actually generate a random number, though I still need more practice and spend more time to completely understand the mechniasm underneath.

--- c_basic_learning.c
+++ c_basic_learning.c
@@ -9,34 +9,56 @@ int f(int x)
 {
     int a = 3; // variable
     int b = 1;
+    int c;  // random number
+    
+    printf("a=%d\n", a);
+    a = 5;
+    printf("a=%d\n", a);
+    printf("c=%d\n", c);
+    
+    if (a == 5)
+    {
+        printf("true\n");
+    }
+    else
+    {
+        printf("false\n");
+    }
     
 	return a*x+b;
 }
 
 int printOddNumber() {
     for (int i=1; i<100; i+=2)
+    {
         printf("%d\n", i);
+    }
     
     return 0;
 }
 
 int printEvenNumber() {
     for (int i=2; i<100; i=i+2)
+    {
         printf("%d\n", i);
-    
+    }
     return 0;
 }
 
 int printFiveXNumber() {
     for (int i=5; i<=100; i+=5)
+    {
         printf("%d\n", i);
+    }
     
     return 0;
 }
 
 int printFiveXNotSevenNumber() {
-    for (int i=5; i<=100; i+=5) {
-        if (i % 7 != 0) {
+    for (int i=5; i<=100; i+=5)
+    {
+        if (i % 7 != 0) 
+        {
             printf("%d\n", i);
         }
     }
@@ -56,12 +78,14 @@ int printFiveXNotTenNumber() {
 
 int main()
 {
+    f(3);
+/*
     printFiveXNumber();
 
     printf("hhhhhhhhhhhh\n");
     
     printFiveXNotTenNumber();
-/*
+
 	for (int x=1; x<10; ++x) {
 		printf("Suri is smart!\t%d:%d\n", x, f(x));
 	}
