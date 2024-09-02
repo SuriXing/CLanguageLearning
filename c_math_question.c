b30058617ab22ea0242a91d25d0f5c0f752d6484
Suri Xing <surixing@outlook.com>
2024-09-02 22:37:13 +0800

    Today I tried to rewrite isPrime and printPrimeNum three times, because I need practice.
    One thing I learnt today is deal with invalid argument: isPrime's argument should be larger than 2, otherwise it's invalid. So I added a if statement at the very beginning.

--- c_math_question.c
+++ c_math_question.c
@@ -12,6 +12,11 @@ int difference(int a, int b)
 
 int isPrime(int num)
 {
+    if (num <= 1)
+    {
+        return 0;
+    }
+
     for (int i=2; i<=num/2; i=i+1)
     {
         if (num%i == 0)
@@ -19,6 +24,59 @@ int isPrime(int num)
             return 0;
         }
     }
+
+    return 1;
+}
+
+int isPrime2(int num)
+{
+    for (int n=2; n<=num/2; n=n+1)
+    {
+        if (num%n == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int isPrime3(int Num)
+{
+    for (int k=2; k<=Num/2; k=k+1)
+    {
+        if (Num%k == 0)
+        {
+            return 0;
+        }
+    }
+
+    /*
+    int k=2;
+    if (k <= num/2) {
+        if (num % k == 0) {
+            return 0;
+        }
+    } else {
+        break;
+    }
+    k=k+1;
+    if (k <= num/2) {
+        if (num % k == 0) {
+            return 0;
+        }
+    } else {
+        break;
+    }
+    k=k+1;
+    if (k <= num/2) {
+        if (num % k == 0) {
+            return 0;
+        }
+    } else {
+        break;
+    }
+    ......
+     */
 
     return 1;
 }
@@ -53,6 +111,42 @@ void printPrimeNum(int from, int to)
     printf("%d\n",countOfPrimeNum);
 }
 
+void printPrimeNum3(int from, int to)
+{
+    if (from>=to)
+    {
+        return ;
+    }
+    
+    for (int i=from; i<=to; i=i+1)
+    {
+        if (isPrime(i) == 1)
+        {
+            printf("%d\n",i);
+        }
+    }
+}
+
+void printPrimrNum2(int from, int to)
+{
+    for (int i=from; i<=to; i=i+1)
+    {
+        int j = 0;
+        for (j=2; j<=i/2; j=j+1)
+        {
+            if (i%j == 0)
+            {
+                break;
+            }
+        }
+
+        if (j>i/2)
+        {
+            printf("%d\n", i);
+        }
+    }
+}
+
 void printPrimeNum2(int from, int to)
 {
     for (int i=from; i<=to; i=i+1)
@@ -80,7 +174,7 @@ void printPrimeNum2(int from, int to)
     }
 
     return sumOfOddNumber - sumOfEvenNumber;
- }
+}
 
 int fasterCalcFrom1To100()
 {
@@ -99,18 +193,28 @@ int fasterCalcFrom1To100()
 
 int main()
 {
-    int result3 = fasterCalcFrom1To100();
+    printPrimeNum(1,234);
+
+    printPrimeNum2(1,234);
+
+    printPrimeNum3(1,765);
+/*    
+    int result = isPrime2(23437);
+    printf("%d\n", result);
+    result3 = fasterCalcFrom1To100();
     printf("%d\n", result3); 
     
     int result = calcFrom1To100();
     printf("%d\n", result);
-//    printPrimeNum(1, 1000);
+    printPrimeNum(1, 1000);
 
     result = Sum(35, 78);
 
     int result2;
     result2 = Sum(35, 78);
     
+
 	return 0;
+*/
 }
 
