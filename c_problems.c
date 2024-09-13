8f68b012f40bd7283a21b02cbd0260a3eff480cb
Suri <surixing@outlook.com>
2024-09-13 22:16:08 +0800

    Today I implemented swapInteger, printFibonacci, and printFactorialFormula
    I'm faster than last time, though still stuck on three variables's relationship in fibonacci. But now I am a little better!
    
    Tomorrow I will try two more functions: GCD and LCM

--- c_problems.c
+++ c_problems.c
@@ -12,7 +12,61 @@ void printTimesTable()
     }
 }
 
+// 1. Fibonacci
+// 2. gcd + lcm
+// 3. !
+
+void printFibonacci(int count)
+{
+    if (count <= 2)
+    {
+        return;
+    }
+
+    int a1 = 1;
+    int a2 = 1;
+    int a3 = a1 + a2;
+    
+    for (int i = 1; i <= count; i += 1)
+    {
+        printf("%d + %d = %d\n", a1, a2, a3);
+    
+    a1 = a2;
+    a2 = a3;
+    a3 = a1 + a2;
+    }
+}
+
+void swapInt(int a, int b)
+{
+    int temp = 0;
+    printf("%d, %d\n", a, b);
+    temp = a;
+    a = b;
+    b = temp;
+    printf("%d, %d\n", a, b);
+}
+
+int factorialFormula(int num)
+{
+    if (num <= 1)
+    {
+        return -1;
+    }
+    
+    for(int i = num -1; i >= 1; i -= 1 )
+    {
+        num = num * i;
+    }
+    
+    return num;
+}
+
 int main()
 {
-    printTimesTable();
+    int result = factorialFormula(5);
+    printf("%d\n", result);
+//   swapInt(23, 543);
+//    printFibonacci(9);
+//printTimesTable();
 }
