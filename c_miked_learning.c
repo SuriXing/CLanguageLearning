ec057b657a5af12765e577063b2876bc9cdd7d8e
Suri <surixing@outlook.com>
2024-10-09 21:41:15 +0800

    Today I followed Mike D's 7th lesson- I learnt that when your initializing characters you'll have to add single quotation marks around the specific character you chose. Also, I learnt that you'll have to add """"around an array made of characters.

--- c_miked_learning.c
+++ c_miked_learning.c
@@ -3,14 +3,19 @@
 
 int main()
 {
-    char characterName[] = "Cookie";
+    int number = 12;
+    double gpa = 4.0;
+    char grade = 'A';
+    char schoolGrade[] = "mastering";
+    
+    /*char characterName[] = "Cookie";
     int characterAge = 15;
     printf("His name is %s.\n", characterName);
     printf("He is %d minutes old.\n", characterAge);
     printf("He really liked his name, %s.\n", characterName);
     printf("But he hates being %d minutes old because it's his unlucky number.\n", characterAge);
 
-    /*char test[100] = "";
+    char test[100] = "";
 
     sprintf(test, "%d minutes", characterAge);
 
