1b50284b98c551ac629e18634c0d09c5eea2db84
Suri <surixing@outlook.com>
2024-12-30 21:14:37 +0800

    I started following Jason Fedin's C Language lessons now. For the first lesson, he taught about variables and memory-I first thought it is very easy, but as he talked about RAS and connected it to bits and bytes (which I've learned about before learning C language) I started to notice that his lessons aren't as easy as I thought it was...

---
+++ c_courses_learning/c_jason_fedin.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int jasonFedin1stLesson()
+{
+    int jason = 6;
+
+    jason = 8;
+
+    return jason;
+}
+
+int main()
+{
+    int jason = jasonFedin1stLesson();
+    printf("%d\n", jason);
+    return 0;
+}
