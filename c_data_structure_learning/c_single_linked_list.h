1a591484a4bce65d5c1c2a570da7c60ec8a760d7
Suri <surixing@outlook.com>
2025-04-05 23:00:42 +0800

    Today I started to create separate header files for my data strucures. I did a while ago, so I re-read the code I wrote and copied the implementation

---
+++ c_data_structure_learning/c_single_linked_list.h
@@ -0,0 +1,6 @@
+#ifndef __C_SINGLE_LINKED_LIST_H__
+#define __C_SINGLE_LINKED_LIST_H__
+
+int c_single_linked_list_main();
+
+#endif // __C_SINGLE_LINKED_LIST_H__
--- c_data_structure_learning/main.c
+++ c_data_structure_learning/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include "c_single_linked_list.h"
 
 int main()
 {
+	c_single_linked_list_main();
+
 	return 0;
 }
