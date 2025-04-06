f3c8b288dcf096a9753f3bb3fb1838bddd0ad975
Suri <surixing@outlook.com>
2025-04-06 21:35:29 +0800

    Today I added the double-linked list .h and added it to main.

---
+++ c_data_structure_learning/c_double_linked_list.h
@@ -0,0 +1,6 @@
+#ifndef __C_DOUBLE_LINKED_LIST_H__
+#define __C_DOUBLE_LINKED_LIST_H__
+
+int c_double_linked_list_main();
+
+#endif // __C_SINGLE_LINKED_LIST_H__
--- c_data_structure_learning/main.c
+++ c_data_structure_learning/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "c_single_linked_list.h"
+#include "c_double_linked_list.h"
 
 int main()
 {
 	c_single_linked_list_main();
+	c_double_linked_list_main();
 
 	return 0;
 }
