800831c52fc41f174733666a1b9deb72a4273dad
Suri <surixing@outlook.com>
2025-04-10 22:25:43 +0800

    Today I implemented a few functions to the array file and fixed some bugs and that's all:(

--- c_data_structure_learning/c_array.c
+++ c_data_structure_learning/c_array.c
@@ -2,3 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include "c_isb_students.h"
+
+IsbStudent* createStudentArray(IsbStudent* student)
+{
+    char arr[265];
+
+    for (int i = 0; ; i++)
+    {
+        arr += student[i];
+    }
+
+    return arr;
+}
+
+int array_main()
+{
+    return 0;
+}
