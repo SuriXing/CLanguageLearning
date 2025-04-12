38af4f0479c7b00d6d40badc09827327e25471d5
Suri <surixing@outlook.com>
2025-04-12 22:23:42 +0800

    Today I inserted print array - 9 errors pop up!!! Liek wth? I just wrote one function and 9 error already? And then I noticed that I called the wrong file:( but also :) cuz there aren't any errors

--- c_data_structure_learning/c_array.c
+++ c_data_structure_learning/c_array.c
@@ -2,35 +2,57 @@
 #include <stdlib.h>
 #include <string.h>
 #include "c_isb_students.h"
+#include <assert.h>
 
-IsbStudent* copyStudentArray(IsbStudent* student)
+char* reverseArr(char* student, int count)
 {
-    char arr[265];
+    assert(NULL != student);
+    assert(count > 0);
 
-    for (int i = 0; ; i++)
+    if (count <= 0)
     {
-        arr += student[i];
+        return NULL;
     }
 
-    return arr;
-}
-
-IsbStudent* reverseArr(IsbStudent* student, int count)
-{
-    char copyArr[256] = *student;
+    char temp;
 
     for (int i = 0; i < count/2; i++)
     {
-        for (int j = count; j > count/2; j++)
-        {
-            copyArr[i] = student[j];
-        }
+        temp = student[count - i - 1];
+        student[count - i - 1] = student[i];
+        student[i] = temp;
     }
 
-    return copyArr;
+    return student;
 }
 
-int array_main()
+IsbStudent* printArr(char* student, int count)
 {
+    assert(NULL != student);
+    assert(count > 0);
+
+    if ((count <= 0) || (NULL == student))
+    {
+        return NULL;
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        printf("%c", student[i]);
+        if (i < count - 1)
+        {    
+            printf(",");
+        }
+    }
+    printf("\n");
+    
+    return 0;
+}
+
+int c_array_main()
+{   
+    char student[4] = {"S" "A" "R"};
+
+    printArr(student, 4);
     return 0;
 }
--- c_data_structure_learning/main.c
+++ c_data_structure_learning/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "c_single_linked_list.h"
 #include "c_double_linked_list.h"
+#include "c_array.c"
 
 int main()
 {
 	c_single_linked_list_main();
 	c_double_linked_list_main();
+	c_array_main();
 
 	return 0;
 }
